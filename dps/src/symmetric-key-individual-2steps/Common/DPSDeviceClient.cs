5e3e5e7caa93e6b30eb646ee6322911b3b450cfe
Michael SH Chi <michi@microsoft.com>
2019-08-11 07:09:31 +0800

    new device provisioning process

--- dps/src/symmetric-key-individual-2steps/Common/DPSDeviceClient.cs
+++ dps/src/symmetric-key-individual-2steps/Common/DPSDeviceClient.cs
@@ -9,23 +9,18 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Microsoft.Azure.Devices.Provisioning.Client.Transport;
 
 namespace Microsoft.Azure.Devices.Provisioning.Client.Samples
 {
     public class DPSDeviceClient
     {
+        private const string GlobalDeviceEndpoint = "global.azure-devices-provisioning.net";
+
         bool _sendTelemetry = true;
-        DeviceAuthenticationWithRegistrySymmetricKey _auth = null;
-        string _iotHub = string.Empty;
-        string _connectionString = string.Empty;
-        public DPSDeviceClient(string iotHubName, DeviceAuthenticationWithRegistrySymmetricKey auth)
-        {
-            _auth = auth;
-            _iotHub = iotHubName;
-        }
-        public DPSDeviceClient(string connString)
-        {
-            _connectionString = connString;
+        DataStorage _storage = null; 
+        public DPSDeviceClient(DataStorage storage){
+            _storage = storage;
         }
         
         #region Direct Methods
@@ -38,26 +33,9 @@ namespace Microsoft.Azure.Devices.Provisioning.Client.Samples
         #endregion
 
         #region Telemetry loop
-        private DeviceClient CreateDeviceClient(){
-            DeviceClient client = null;
-
-            if(!string.IsNullOrEmpty(_iotHub) && _auth != null){
-                Console.WriteLine($"CreateDeviceClient::{_iotHub}");
-                client = DeviceClient.Create(_iotHub, _auth, TransportType.Mqtt_WebSocket_Only);
-            }else if(!string.IsNullOrEmpty(_connectionString)){
-                Console.WriteLine($"CreateDeviceClient::{_connectionString}");
-                client = DeviceClient.CreateFromConnectionString(_connectionString);
-            }else{
-                throw new ArgumentNullException("Connection information");
-            }
-            //  TODO: Should handle reprovision ? If so, then should switch to 
-            //          DeviceProvisioningClient instead of DeviceClient to create IoT hub connection
-            //client.SetMethodHandlerAsync("Reprovision", ReprovisionHandler, null).GetAwaiter().GetResult();
-            client.SetDesiredPropertyUpdateCallbackAsync(DesiredPropertyUpdated, null).GetAwaiter().GetResult();
-            return client;
-        }
-        private async Task SendTelemetryAsync(){
-            using (DeviceClient iotClient = CreateDeviceClient())
+        private async Task SendTelemetryAsync(DeviceClient iotClient)
+        {
+            using (iotClient)
             {
                 Console.WriteLine("DeviceClient OpenAsync.");
                 await iotClient.OpenAsync().ConfigureAwait(false);
@@ -69,7 +47,6 @@ namespace Microsoft.Azure.Devices.Provisioning.Client.Samples
                 var telemetryGenerator = new RandomTelemetry();
                 while( i ++ < 100 && _sendTelemetry){
                     Console.WriteLine("DeviceClient SendEventAsync.");
-#if true
                     foreach(var line in telemetryGenerator.GenerateTelemetryLines())
                     {
                         var message = new Message(Encoding.UTF8.GetBytes(line));
@@ -79,26 +56,51 @@ namespace Microsoft.Azure.Devices.Provisioning.Client.Samples
                         await iotClient.SendEventAsync(message).ConfigureAwait(false);
                         await Task.Delay(1000 * 3);
                     }
-#else
-                    var body = telemetryGenerator.GenerateTelemetry();
 
-                    var message = new Message(Encoding.UTF8.GetBytes(body));
-                    message.ContentType = "application/JSON";
-                    message.ContentEncoding = "utf-8";
-                    await iotClient.SendEventAsync(message).ConfigureAwait(false);
-                    await Task.Delay(1000 * 3);
-#endif
                 }
                 Console.WriteLine("DeviceClient CloseAsync.");
                 await iotClient.CloseAsync().ConfigureAwait(false);
             }       
         }
         #endregion
+
+        public async Task<Microsoft.Azure.Devices.Client.DeviceClient> ProvisionAsync<DeviceClient>(DataStorage storage)
+        {
+            var connStringContent = storage.Read("ConnectionString");
+            DevicePortalInfoModel model = JsonConvert.DeserializeObject<DevicePortalInfoModel>(connStringContent);
+            string scope = model.Device.ID_Scope;
+            string primaryKey = model.Device.PrimaryKey;
+            string secondaryKey = model.Device.SecondaryKey;
+            string registrationId = model.Device.RegistrationId;
+            
+            _sendTelemetry = false;
+
+            using (var security = new SecurityProviderSymmetricKey(registrationId, primaryKey, secondaryKey))
+            {
+                using (var transport = new ProvisioningTransportHandlerAmqp(TransportFallbackType.TcpOnly))
+                {
+                    ProvisioningDeviceClient provClient =
+                        ProvisioningDeviceClient.Create(GlobalDeviceEndpoint, scope, security, transport);
+                    DeviceRegistrationResult result = await provClient.RegisterAsync().ConfigureAwait(false);
+                    
+                    if (result.Status != ProvisioningRegistrationStatusType.Assigned)
+                        return null;
+                    Console.WriteLine($"Provisioninged device : {result.DeviceId} at {result.AssignedHub}");
+                    var client = Microsoft.Azure.Devices.Client.DeviceClient.Create(result.AssignedHub, model.Auth, TransportType.Amqp);
+                    client.SetMethodHandlerAsync("Reprovision", ReprovisionHandler, null).GetAwaiter().GetResult();
+                    client.SetDesiredPropertyUpdateCallbackAsync(DesiredPropertyUpdated, null).GetAwaiter().GetResult();
+                    
+                    //  TODO:Save new connection information
+                    
+                    return client;
+                }
+            }
+        }
         public async Task StartAsync()
         {
             _sendTelemetry = false;
-
-            await SendTelemetryAsync();
+            var iotClient = await ProvisionAsync<DeviceClient>(_storage);
+            await SendTelemetryAsync(iotClient);
         }
         #region Device Twins
         private Task DesiredPropertyUpdated(TwinCollection desiredProperties, object userContext){
--- dps/src/symmetric-key-individual-2steps/Common/DataStorage.cs
+++ dps/src/symmetric-key-individual-2steps/Common/DataStorage.cs
@@ -7,7 +7,6 @@ using Newtonsoft.Json;
 namespace Microsoft.Azure.Devices.Provisioning.Client.Samples
 {
     public class DataStorage {
-        private readonly string _dbFile = "../../conn.txt";
         private readonly string _conn = "../../secure_conn.json";
         public string Read(string key){
             switch(key){
