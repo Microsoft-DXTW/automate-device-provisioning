3f190775647f993ef26b9094996c3534b7c4e6c3
Michael SH Chi <michi@microsoft.com>
2019-08-13 11:21:22 +0800

    update

---
+++ dps/src/symmetric-key-individual-2steps/Common/DataStorage.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Microsoft.Azure.Devices.Provisioning.Client.Samples
+{
+    public class DataStorage {
+        private readonly string _conn = "../../secure_conn.json";
+        public string Read(string key){
+            switch(key){
+                case "ConnectionString":
+                    return System.IO.File.ReadAllText(_conn);
+            }
+            return null;
+        }
+    }
+}
---
+++ dps/src/symmetric-key-individual-2steps/Program.cs
@@ -0,0 +1,56 @@
+﻿﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Azure.Devices.Provisioning.Client;
+using Microsoft.Azure.Devices.Provisioning.Client.Samples;
+using Microsoft.Azure.Devices.Provisioning.Client.Transport;
+using Microsoft.Azure.Devices.Shared;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Microsoft.Azure.Devices.Client;
+namespace SymmetricKeySample
+{
+    class Program
+    {
+        private static IAuthenticationMethod GetAuthenticationMethod(){
+            string json = new DataStorage().Read("AuthenticationMethod");
+            if(!string.IsNullOrEmpty(json)){
+                IAuthenticationMethod auth = JsonConvert.DeserializeObject<DeviceAuthenticationWithRegistrySymmetricKey>(json);
+                return auth;
+            }else{
+                return null;
+            }
+        }
+        
+        private static void ReadJsonConfiguration(){
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+        }
+        public static int Main(string[] args)
+        {
+            ReadJsonConfiguration();
+            
+            var db = new DataStorage();
+            var secureConnection = db.Read("ConnectionString");
+            DevicePortalInfoModel o = JsonConvert.DeserializeObject<DevicePortalInfoModel>(secureConnection);
+            DeviceAuthenticationWithRegistrySymmetricKey auth = (DeviceAuthenticationWithRegistrySymmetricKey)o.Auth;
+            string iotHub = (string)o.Host;
+            DPSDeviceClient sample = null;
+                      
+            Console.WriteLine($"Connecting to IoT Hub with AuthenticationMethod:{iotHub}");
+            sample = new DPSDeviceClient(iotHub, auth);
+            
+            sample.StartAsync().GetAwaiter().GetResult();
+
+            Console.WriteLine("Enter any key to exit");
+            Console.ReadLine();
+            return 0;
+        }
+    }
+}
