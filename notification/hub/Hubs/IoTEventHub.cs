45762c2a4b5f9e25ed25f5af7a26ad5a5f893a97
Michael SH Chi <michi@microsoft.com>
2019-08-24 17:47:06 +0800

    add signalr server/client

---
+++ notification/hub/Hubs/IoTEventHub.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace hub.Hubs
+{
+
+    public class IoTEventHub : Hub
+    {
+        public void NotifyUsers(string message){
+            Clients.Client(Context.ConnectionId).SendAsync("notifyusers", $"sending {message}");
+        }
+        // private void NotifyUsers(string group, string message){
+        //     Clients.Group(group).SendAsync("notifyusers", message);
+        // }
+        public void Echo(string name, string message)
+        {
+            Clients.Client(Context.ConnectionId).SendAsync("echo", name, message + " (echo from server)");
+        }
+    }
+}
---
+++ notification/readme.md
@@ -0,0 +1,3 @@
+## SignalR Reference
+
+-   https://docs.microsoft.com/zh-tw/azure/azure-signalr/signalr-quickstart-dotnet-core
